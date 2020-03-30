2db1b1939b77bc371a930fc9a5d0bbf51c13cd43
Angeline R <4ng133@gmail.com>
2020-03-30 11:46:27 +0100

    Connect to database

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
+const connection = require("../../helpers/db");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.post("/signup", function(req, res, next) {
-  res.send("I am in POST signup");
+  const user = {
+    email: req.body.email,
+    password: req.body.password,
+    name: req.body.name,
+    lastname: req.body.lastname
+  };
+
+  connection.query("INSERT INTO users SET ?", user, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      res.status(200).json(user);
+      res.end();
+    }
+  });
 });
 
 module.exports = router;
